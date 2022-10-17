bec28b9ee625247d4deaeca2a781d9484a2df15e
Hector Rojas <hrpxa270@hotmail.com>
2022-10-16 19:37:25 -0500

    Feature 1 ready

--- App.js
+++ App.js
@@ -6,52 +6,11 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {Component} from 'react';
 import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  useColorScheme,
-  Text,
-  View, TextInput,
-} from "react-native";
 
-import {
-  Colors,
-  //   DebugInstructions,
-  //   Header,
-  //   LearnMoreLinks,
-  //   ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import Greeting from './components/Greeting';
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <View style={styles.container}>
-      <TextInput value="Hector Rojas" />
-      <Text style={styles.text}>Welcome to this CI/CD</Text>
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 30,
-    color: 'red',
-  },
-});
-
-export default App;
+export default function App() {
+  return <Greeting />;
+}
---
+++ components/Greeting.js
@@ -0,0 +1,57 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {Component} from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+} from 'react-native';
+
+export default class Greeting extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={{
+            height: 40,
+            borderColor: 'gray',
+            borderWidth: 1,
+          }}
+          placeholder="Write your Name..."
+          onChangeText={name => this.setState({name})}
+        />
+        <Text style={{marginTop: 10}}>Welcome to this CI/CD</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 40,
+    marginHorizontal: 16,
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'stretch',
+    justifyContent: 'flex-start',
+  },
+});
